92a780912671b1d095a9eb3b09b2dcb636a195d6
Bhagyarekha <bhagyarekha.deshpande@gmail.com>
2022-09-05 15:27:17 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+
+const loginBtn = document.getElementById("loginBtn");
+// const submitBtn = document.getElementById("submitBtn");
+
+
+loginBtn.addEventListener("click", function(e){
+  e.preventDefault();
+  window.location.href = "trees.html";
+  return false; 
+ });
+
+ 
